18efb21cc9e597e1f1a0dec395992e799b2556e8
Sh1nX0 <116954589+Sh1nX0@users.noreply.github.com>
2023-01-21 13:55:38 +0600

    Add files via upload

---
+++ app/src/main/java/com/example/myapplication8/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.myapplication8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView listView = findViewById(R.id.listview);
+        listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titles));
+        listView.setTextFilterEnabled(true);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView<?> a, View v, int position, long id){
+                Intent intent =new Intent();
+                intent.setClass(MainActivity.this, DetailActivity.class);
+                intent.putExtra("title", position);
+                startActivity(intent);
+            }
+        });
+    }
+    private String titles[]={
+            "Январь",
+            "Февраль",
+            "Март",
+            "Апрель",
+            "Май",
+            "Июнь",
+            "Июль",
+            "Август",
+            "Сентябрь",
+            "Октябрь",
+            "Ноябрь",
+            "Декабрь",
+    };
+}
